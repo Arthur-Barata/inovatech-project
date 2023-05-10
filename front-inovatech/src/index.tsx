8dab7bc65f00a16172f1470108bc758921593398
kmlrp <kmlrp@motorola.com>
2023-05-10 19:27:13 -0400

    ATUALIZAÇAO-FRONT-END

--- front-inovatech/src/index.tsx
+++ front-inovatech/src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { GlobalStyles, ThemeProvider, Typography } from '@mui/material';
+import ReactDOM from 'react-dom/client';
+import { globalStyle, theme } from './styles/global-styles';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyles styles={globalStyle} />
+    <Typography component={'div'}>
+      <App />
+    </Typography>
+  </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
